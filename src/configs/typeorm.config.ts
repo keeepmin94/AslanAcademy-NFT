49d94d35e8a6c927dc6de6fc9d7f2a48669038d2
keeepmin94 <wjdwkjh94@gmail.com>
2023-07-21 23:17:02 +0900

    to local container connect

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'host.docker.internal', //'localhost',
   port: 3306,
   username: 'root',
   password: 'test1234',
