81be778ec1d322526dbdc687504942cb8f2ac908
keeepmin94 <wjdwkjh94@gmail.com>
2023-10-16 18:00:42 +0900

    fix docker-compose file

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -8,5 +8,5 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   password: 'test1234',
   database: 'AslanTest',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: false, //true값을 주면 애플리케이션을 다시 실행할 떄 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해줍니다.
+  synchronize: true, //true값을 주면 애플리케이션을 다시 실행할 떄 엔티티안에서 수정된 컬럼의 길이 타입 변경값등을 해당 테이블을 Drop한 후 다시 생성해줍니다.
 };
